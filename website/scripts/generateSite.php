c0f87df46351acc69b9754e9326127dbc716f92c
David Matejka <matej21@matej21.cz>
2021-11-25 22:31:58 +0100

    ssg every page

--- website/scripts/generateSite.php
+++ website/scripts/generateSite.php
@@ -11,17 +11,29 @@ $container->callMethod(function (
 	$db->query('set search_path to stage_live');
 	$baseUri = new \Nette\Http\UrlScript('http://ssg.localhost/');
 	$outDir = __DIR__ . '/../dist/';
+	$requests = [
+		'Homepage' => [[]],
+		'Category' => [['slug' => 'nette'], ['slug' => 'java-script'], ['slug' => 'graph-ql']],
+		'Article' => [['slug' => 'nette-ssg']],
+	];
+	foreach ($requests as $presenterName => $requestParams) {
+		foreach ($requestParams as $params) {
+			$appRequest = new \Nette\Application\Request($presenterName, 'GET', $params);
+			$presenter = $presenterFactory->createPresenter($appRequest->getPresenterName());
+			$presenter->autoCanonicalize = false; // it returns a redirect otherwise
+			$response = $presenter->run($appRequest);
+			assert($response instanceof \Nette\Application\Responses\TextResponse);
+			$html = (string) $response->getSource();
+			$url = $router->constructUrl([
+				'presenter' => $appRequest->getPresenterName(),
+			] + $appRequest->getParameters(), $baseUri);
+			assert($url !== null);
+			$pagePath = substr($url, strlen($baseUri->getBaseUrl()));
+			dump($pagePath); // :-( contains ?slug=xxx
+			echo($html);
 
-	$presenter = $presenterFactory->createPresenter('Homepage');
-	$appRequest = new \Nette\Application\Request('Homepage', 'GET', []);
-	$response = $presenter->run($appRequest);
-	assert($response instanceof \Nette\Application\Responses\TextResponse);
-	$html = (string) $response->getSource();
-	$url = $router->constructUrl([
-		'presenter' => $appRequest->getPresenterName(),
-	] + $appRequest->getParameters(), $baseUri);
-	assert($url !== null);
-	$pagePath = substr($url, strlen($baseUri->getBaseUrl()));
-	echo "$pagePath\n";
-	file_put_contents($outDir . $pagePath . '/index.html', $html);
+//			file_put_contents($outDir . $pagePath . '/index.html', $html);
+
+		}
+	}
 });
