a6ca067ce4fffb5e2a1d2afd91124208f04cfe5c
David Matejka <matej21@matej21.cz>
2021-11-25 22:17:44 +0100

    implement try-file in index.php

--- website/public/index.php
+++ website/public/index.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+if (PHP_SAPI === 'cli-server') {
+	$_SERVER['SCRIPT_NAME'] = '/index.php';
+	if ($_SERVER['REQUEST_URI'] !== '/' && is_file($_SERVER['DOCUMENT_ROOT'] . urldecode($_SERVER['REQUEST_URI']))) {
+		return false;
+	}
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = App\Bootstrap::boot();
